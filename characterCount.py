a146d113497f2ffefb078e67f3532c3fd142de07
prasurjya <65493249+PrasurjyaWhiteHatJr@users.noreply.github.com>
2021-05-26 13:08:14 +0530

    Add files via upload

---
+++ characterCount.py
@@ -0,0 +1,13 @@
+intro=input("Enter The A String")
+characterCount=0
+wordCount=1
+for i in intro:
+    characterCount=characterCount+1
+    if(i== ' '):
+        wordCount=wordCount+1
+print("Number Of Words In The String")
+print(wordCount)
+print("Number Of Characters In The String:")
+print(characterCount)
+
+#My Name Is Prasurjya
---
+++ greet.py
@@ -0,0 +1,4 @@
+def greet(Name):
+    print("Hello, "+ Name+". How Are You?")
+
+greet("Prasurjya") 
